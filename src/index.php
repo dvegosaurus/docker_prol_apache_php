bd4b05466702ecdb8e18b9a4efd4291e1f35921e
root <root@localhost.localdomain>
2023-03-12 19:04:22 +0100

    first commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$value = "World";
+
+$db = new PDO('mysql:host=database;dbname=mydb;charset=utf8mb4', 'myuser', 'secret');
+
+$databaseTest = ($db->query('SELECT * FROM dockerSample'))->fetchAll(PDO::FETCH_OBJ);
+$h = gethostname()
+?>
+
+<html>
+    <body>
+        <h1>Hello, <?= $value ?>!</h1>
+            
+        <?php foreach($databaseTest as $row): ?>
+            <p>Hello, <?= $row->name ?></p>
+        <?php endforeach; ?>
+        <h2>hostname : <?= $h?></h2>
+    </body>
+</html>
